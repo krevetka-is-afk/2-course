a9c355acfc6d3a32b2c692a1d8f0c2f7c8c575e4
Rastvorov Sergei <144595454+krevetka-is-afk@users.noreply.github.com>
2024-10-17 15:21:57 +0300

    Update ДЗ_5.s

--- АВС/ДЗ_5/ДЗ_5.s
+++ АВС/ДЗ_5/ДЗ_5.s
@@ -4,6 +4,8 @@ msg_newline:	.asciz "\n"
 msg_res_C:	.asciz "Max arg for factorial in CYCLE is: "
 msg_res_R:	.asciz "Max arg for factorial in RECURSION is: "
 msg_error:	.asciz "Wrong option\nTry again 1 or 2: "
+msg_rec_start:  .asciz "Recursion starts!"
+msg_rec_stop:  .asciz "Recursion stop!"
 max_32bit_word:	.word 2147483647
 
 .text
@@ -12,7 +14,7 @@ main:
 	la a0, msg_start	
 	li a7, 4
 	ecall
-	
+error_r:
 	jal input_option
 	j check_option
 	
@@ -41,7 +43,7 @@ error:
 	la a0, msg_error
 	li a7, 4
 	ecall
-	j main
+	j error_r
 
 run_cycle:
 	jal fact_max_cycle
@@ -57,10 +59,19 @@ run_cycle:
 	j end
 
 run_recursion:
-	li t0, 1
-	li t1, 1
-	jal fact_max_recursion
+	li t0, 1		# counter
+	li t1, 1		# fact curr
 	
+	# debug
+	#la a0, msg_rec_start
+	#li a7, 4
+	#ecall
+	#la a0, msg_newline
+        #li a7, 4
+        #ecall
+	
+	jal fact_max_recursion
+stop_rec:	
 	la a0, msg_res_R
 	li a7, 4
 	ecall
@@ -72,8 +83,8 @@ run_recursion:
 	j end
 
 fact_max_cycle:
-	li t0, 1 		
-	li t1, 1		
+	li t0, 1 		# counter	
+	li t1, 1		# fact curr
 	la t2, max_32bit_word	
 	j cycle_loop
 	
@@ -92,16 +103,29 @@ end_cycle_loop:
 	ret
 	
 fact_max_recursion:
- 	mul t2, t1, t0		
-    la t3, max_32bit_word	
- 	bge t2, t3, end_factorial_recursive	
-    addi t0, t0, 1		
-    mv t1, t2		
-    jal fact_max_recursion	
+ 	mul t2, t1, t0	
+    	la t3, max_32bit_word	
+ 	blt t3, t2, end_factorial_recursive	
+    	addi t0, t0, 1
+    	#blt t2, t1, end_factorial_recursive
+    	mv t1, t2	
+	
+	# debug  	
+    	#mv a0, t0
+        #li a7, 1
+        #ecall
+        #la a0, msg_newline
+        #li a7, 4
+        #ecall
+    		
+    	jal fact_max_recursion	
 
 end_factorial_recursive:
-    	addi t0, t0, -1		
-    	ret                    
+    	addi t0, t0, -1	
+    	#la a0, msg_rec_stop
+    	#li a7, 4
+    	#ecall
+    	j stop_rec              
 
 end:
 	la a0, msg_newline
